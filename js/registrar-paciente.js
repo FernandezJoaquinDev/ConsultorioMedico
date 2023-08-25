e40259f23f1de2f6979187d2e522e5dafa6d7cf1
Joaquin Fernandez <crashnebula21@gmail.com>
2023-08-24 22:54:52 -0300

    integracion del modal de pacientes (no terminado)

--- js/registrar-paciente.js
+++ js/registrar-paciente.js
@@ -41,23 +41,39 @@ const agregarPacientes = (event) =>{
 //Agregar un navbar
 //Crear la funcion listar pacientes (con modal)
 
-const contenedorT=document.getElementById("contenedorModal");
+const contenedorT=document.getElementById("contenedorPacientes");
+const prueba=document.getElementById("contenedorPrueba");
 
 const listarPacientes = (event) =>{
-    event.preventDefault();
+     event.preventDefault();
+     contenedorT.innerHTML=<div class="modal-dialog" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLabel">Lista de Pacientes</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body row" id="contenedorPacientes">
+           
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+       </div>
+     </div>
+   </div>;
+     
     // window.location.href="/pages/listadoPacientes.html";
     // window.location.assign("/pages/listadoPacientes.html")
     pacientes=JSON.parse(localStorage.getItem(`pacientes`))
     pacientes.map((item)=>{
         let columna = document.createElement("div");
-        columna.classList="row";
+        columna.classList="col-12";
         let tarjeta = `<div class="card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">${item.apeynom}</h5>
           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
         </div>
       </div>`;
       columna.innerHTML=tarjeta;
